676d5de34b01e00a9c9458cae8ef82f7835f3f07
amsully <sullivan1513@gmail.com>
2016-04-24 18:31:24 -0400

    python3 bot working , java bot working, python bot working

--- local_run.sh
+++ local_run.sh
@@ -4,4 +4,4 @@ cd ..
 cd blockbattle-engine
 javac -d bin/ `find ./ -name '*.java' -regex '^[./A-Za-z0-9]*$'`
 
-java -cp bin com.theaigames.blockbattle.Blockbattle "../block-battle-amsully/BotRun.py" "../block-battle-amsully/BotRun.py" 2>err.txt 1>out.txt
+java -cp bin com.theaigames.blockbattle.Blockbattle "python ../block-battle-amsully/BotRun.py" "python ../block-battle-amsully/BotRun.py" 2>err.txt 1>out.txt
